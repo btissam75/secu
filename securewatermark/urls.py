5f53fb681437b20d492157214196260259fba385
btissam75 <arehalbtissam07@gmail.com>
2025-06-04 14:26:26 +0100

    projet de securite

---
+++ securewatermark/urls.py
@@ -0,0 +1,63 @@
+# securewatermark/urls.py
+from django.contrib import admin
+from django.urls import path, include, reverse_lazy
+from watermark import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # 1) Formulaire « Entrez votre e-mail pour réinitialiser »
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html',
+            email_template_name='registration/password_reset_email.html',
+            subject_template_name='registration/password_reset_subject.txt',
+            success_url=reverse_lazy('password_reset_done'),
+        ),
+        name='password_reset'
+    ),
+
+    # 2) Page de confirmation « On a envoyé l’e-mail »
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    # 3) Lien cliqué depuis le mail : choisir un nouveau mot de passe
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html',
+            success_url=reverse_lazy('password_reset_complete')
+        ),
+        name='password_reset_confirm'
+    ),
+
+    # 4) Page « Votre mot de passe a été modifié »
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+
+  path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    # path('accounts/', include('allauth.urls')),
+    path('', include('watermark.urls')),
+    
+
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('upload/', views.upload_document, name='upload'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
